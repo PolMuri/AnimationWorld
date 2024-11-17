4e6e1a2cd740cc95994bcf99b8b9d46ce3b2ec56
Pol Murillas <109922379+PolMuri@users.noreply.github.com>
2024-11-17 21:21:20 +0100

    Update cartoon_list.php

--- src/Practica_Animation/cartoon_list.php
+++ src/Practica_Animation/cartoon_list.php
@@ -26,21 +26,21 @@
     die('No es pot connectar: ' . mysqli_error()); 
 } 
 
-//Agafo amb post el valor dels camps del formulari html i ho guardo a les variables que estic creant 
+// Agafo amb post el valor dels camps del formulari html i ho guardo a les variables que estic creant 
 
 $Find_cartoon = $_POST["Find_cartoon"];
 
-//ara insereixo els valors del formulari a la taula de la base de dades, s'insereix per nom de variable
+// Ara insereixo els valors del formulari a la taula de la base de dades, s'insereix per nom de variable
 
 
-//Selecciono les columnes i les renombro,uneixo les taules "cartoon" i 
-//"cartoonist" amb l'Inner Join i l'ON per comparar els valors de la columna "id" de les dues taules.
-//Després, uneixo les taules "cartoon" i "film" utilitzant l'Inner Join i l'ON com abans
-//per comparar els valors de la columna "film_id" de la taula "cartoon" i la columna "id" de la taula "film".
-//Al final utilitzo el "WHERE" per filtrar les files que contenen la cadena de caràcters 
-//que hi ha a la variable "$Find_cartoon" en la columna "nom" de la taula "cartoon".
-//Resumint, lligo l'ID de la taula cartoon que es troba a la columna film amb el mateix id de la taula FILM que es diu ID.
-//El mateix faig amb la taula CARTOONIST ja que lligo l'id d'aquesta taula amb la taula CARTOON i l'id que aquesta té a la columna cartoonist
+// Selecciono les columnes i les renombro,uneixo les taules "cartoon" i 
+// "cartoonist" amb l'Inner Join i l'ON per comparar els valors de la columna "id" de les dues taules.
+// Després, uneixo les taules "cartoon" i "film" utilitzant l'Inner Join i l'ON com abans
+// per comparar els valors de la columna "film_id" de la taula "cartoon" i la columna "id" de la taula "film".
+// Al final utilitzo el "WHERE" per filtrar les files que contenen la cadena de caràcters 
+// que hi ha a la variable "$Find_cartoon" en la columna "nom" de la taula "cartoon".
+// Resumint, lligo l'ID de la taula cartoon que es troba a la columna film amb el mateix id de la taula FILM que es diu ID.
+// El mateix faig amb la taula CARTOONIST ja que lligo l'id d'aquesta taula amb la taula CARTOON i l'id que aquesta té a la columna cartoonist
 $sql = "SELECT cartoon.nom AS cartoon_name, cartoonist.nom AS cartoonist_name, film.name AS film_name, cartoon.img AS cartoon_image 
 FROM cartoon 
 INNER JOIN cartoonist ON cartoon.cartoonist = cartoonist.id 
@@ -55,7 +55,7 @@ echo "<fieldset>";
 echo "<legend>Cartoon List</legend>";
 echo "Cadena de caràcters a buscar: $Find_cartoon </br> </br>";
 
-//Controlo que si no hi ha cap coincidencia amb l'string buscat, mostri un missatge dient-ho
+// Controlo que si no hi ha cap coincidencia amb l'string buscat, mostri un missatge dient-ho
 if(mysqli_num_rows($resultat) > 0) {
 	
 echo "<table>";
